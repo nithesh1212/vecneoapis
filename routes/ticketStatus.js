3b21ee6201073ea643116f0116441ea6d631f39c
Nithesh Machiraju <nithesh.machiraju@kore.com>
2017-07-13 16:58:29 +0530

    initial commit for mock apis

---
+++ routes/ticketStatus.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+var Store = require("jfs");
+var db = new Store("data",{pretty:true});
+
+
+
+router.get('/', function(req, res, next) {
+	
+	var ticketNumber = req.query.ticketNumber;
+	
+	//var returndate = req.query.returndate;
+	var obj = db.getSync("ticketDetails");
+	var result = {
+        "data":[]
+    };		
+    	obj.data.map(function(data){
+    		if(data.ticket_number === ticketNumber){
+    			//flight.type = "going";
+    			result.data.push(data);
+    		} /*else if(flight.date === date && flight.departureairport.code === to && flight.arrivalairport.code === from){
+               flight.type = "return";
+               result.flights.push(flight);
+    		}*/
+    	});
+    res.send(result);
+    	
+});
+
+	
+	
+module.exports = router;
